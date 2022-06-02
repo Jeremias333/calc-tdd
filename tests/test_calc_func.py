6f703602f4dbcf08536e9750e2430a156efb4033
jeremias <jeremiasprogrammer@gmail.com>
2022-06-02 11:04:03 -0300

    prepare repository to bdd

--- tests/test_calc_func.py
+++ tests/test_calc_func.py
@@ -1,3 +1,15 @@
+try:
+    import sys
+    import os
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), '..'
+            )
+        )
+    )
+except:
+    raise
 from src import calculator
 
 import pytest
